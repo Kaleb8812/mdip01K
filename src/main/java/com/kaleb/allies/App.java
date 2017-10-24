ae024ce0ef124c7f6b86ac5a93bce131710ade88
kaleb8812 <kaleb.dammeier@gmail.com>
2017-10-24 10:37:41 -0500

    Base end to end unit test

--- src/main/java/com/kaleb/allies/App.java
+++ src/main/java/com/kaleb/allies/App.java
@@ -3,16 +3,22 @@ package com.kaleb.allies;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        FileUtils.readLines(new File(path));
+public class App {
+    public static void main(String[] args) throws IOException {
+    }
+
+    Boolean ReadBookAndStopWords() throws IOException {
+        Boolean listEmpty = true;
+
+        List singleLine = FileUtils.readLines(new File("c:/Workspaces/IDEA/mdip01K/sample.txt"));
+        if (singleLine.isEmpty()) {
+            listEmpty = false;
+        }
+
+        return listEmpty;
     }
 }
