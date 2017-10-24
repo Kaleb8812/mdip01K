e092d9137c9329a77d3997f4370cf2a52b8ca9b9
kaleb8812 <kaleb.dammeier@gmail.com>
2017-10-23 23:19:26 -0500

    Adding Maven

---
+++ src/main/java/com/kaleb/allies/App.java
@@ -0,0 +1,18 @@
+package com.kaleb.allies;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        FileUtils.readLines(new File(path));
+    }
+}
