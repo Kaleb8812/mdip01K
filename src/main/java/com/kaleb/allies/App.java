ac61d64696afce3789ecb54f96c360590b5d5745
kaleb8812 <kaleb.dammeier@gmail.com>
2017-10-31 18:35:20 -0500

    Addition of basic word counting method/dependencies

--- src/main/java/com/kaleb/allies/App.java
+++ src/main/java/com/kaleb/allies/App.java
@@ -5,31 +5,48 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public class App {
     public static void main(String[] args) throws IOException {
     }
 
-    Boolean ReadBookAndStopWords() throws IOException {
+    Boolean readBookAndStopWords() throws IOException {
         Boolean listEmpty = false;
 
         List allLines = FileUtils.readLines(new File("C:/Workspaces/IDEA/mdip01K/src/main/resources/sample.txt"));
+
         if (allLines.isEmpty()) {
             listEmpty = true;
         } else {
-            String singleLine = "";
-            for (Object allLine : allLines) {
-                singleLine = allLine.toString();
-                String[] wordsSplitBySpace = singleLine.split("\\s+");
-                for (String aWordsSplitBySpace : wordsSplitBySpace) {
-                    //Filter against stop word list; Start word frequency counter
-                    System.out.println(aWordsSplitBySpace);
-                }
-            }
-
+            wordCounter(allLines);
         }
 
         return listEmpty;
+    }
+
+    private void wordCounter(List allLines) {
+        HashMap<String, Integer> allLinesMap = new HashMap<>();
+        String singleLine = "";
+        for (Object allLine : allLines) {
+            singleLine = allLine.toString();
+            String[] wordsSplitBySpace = singleLine.split("\\s+");
+
+            for (String aWordsSplitBySpace : wordsSplitBySpace) {
+                //Filter against stop word list; Start word frequency counter
+                if (!allLinesMap.containsKey(aWordsSplitBySpace)) {
+                    allLinesMap.put(aWordsSplitBySpace, 1);
+                } else {
+                    allLinesMap.put(aWordsSplitBySpace, allLinesMap.get(aWordsSplitBySpace) + 1);
+                }
+                System.out.println(aWordsSplitBySpace + "       count: " + allLinesMap.get(aWordsSplitBySpace));
+            }
+        }
     }
 }
--- src/test/java/com/kaleb/allies/AppTest.java
+++ src/test/java/com/kaleb/allies/AppTest.java
@@ -5,6 +5,10 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 public class AppTest extends TestCase {
     public AppTest(String testName) {
@@ -19,8 +23,7 @@ public class AppTest extends TestCase {
     //Test fails if read in List is empty
     public void testApp() throws IOException {
         App appClass = new App();
-        Boolean listNotEmpty = appClass.ReadBookAndStopWords();
-
+        Boolean listNotEmpty = appClass.readBookAndStopWords();
         assertTrue(!listNotEmpty);
     }
 
